49e7880d0ad854814bde341738f9b2286fe98f89
aoiferow <72607771+aoiferow@users.noreply.github.com>
2021-01-25 17:51:46 -0500

    displayWeather Function & started the time function

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
+function displayWeather(response) {
+    console.log (response.data);
+    let currentCity = document.querySelector("#city");
+    let currentTemp = document.querySelector("#temp");
+    let currentCond = document.querySelector("#conditions");
+    let currentHumidity = document.querySelector("#humidity");
+    let currentWind = document.querySelector("#wind");
+    currentCity.innerHTML = response.data.name; 
+    currentTemp.innerHTML = Math.round(response.data.main.temp);
+    currentCond.innerHTML = response.data.weather[0].description;
+    currentHumidity.innerHTML = response.data.main.humidity;
+    currentWind.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "26fb0e2935bcdd02eddacbda042238e7";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
- console.log (apiUrl);
+axios.get(apiUrl).then(displayWeather);
+let now = new Date();
+
+console.log(now);
