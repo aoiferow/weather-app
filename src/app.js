cf94c67fe1ab9074b4ba4576d3acbf334f371e12
aoiferow <72607771+aoiferow@users.noreply.github.com>
2021-01-26 18:43:02 -0500

    Change HTTP to HTTPS, as per netlify

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ function displayWeather(response) {
     currentHumidity.innerHTML = response.data.main.humidity;
     currentWind.innerHTML = Math.round(response.data.wind.speed);
     timeDate.innerHTML = formatDate(response.data.dt * 1000).bold();
-    iconElement.setAttribute ( "src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) 
+    iconElement.setAttribute ( "src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) 
     iconElement.setAttribute ("alt", response.data.weather[0].description)
 }
 
